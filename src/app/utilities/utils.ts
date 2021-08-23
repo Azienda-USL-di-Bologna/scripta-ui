d7c1cb3015ff3a1aa1ff1d9466d68e71844df685
f.gusella <devnull@localhost>
2021-08-23 11:58:13 +0200

    mannaggia

---
+++ src/app/utilities/utils.ts
@@ -0,0 +1,6 @@
+
+export class Utils {
+  public static dateFormatter(date: Date) {
+    return date.getDate().toString().padStart(2, '0') + "/" + (date.getMonth() + 1).toString().padStart(2, '0') + "/" + date.getFullYear()
+  }
+}
