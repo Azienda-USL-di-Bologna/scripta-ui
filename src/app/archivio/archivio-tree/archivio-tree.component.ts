08fc119214769c46906983f170c7142dda6e2391
l.salomone <devnull@localhost>
2022-02-11 11:10:56 +0100

    anche abbastanza stilizzato, ma i figli...

--- src/app/archivio/archivio-tree/archivio-tree.component.ts
+++ src/app/archivio/archivio-tree/archivio-tree.component.ts
@@ -4,6 +4,7 @@ import { TreeNode } from 'primeng/api';
 import { ArchivioService } from '../archivio.service';
 import { Archivio, ArchivioDetail, ENTITIES_STRUCTURE } from '@bds/ng-internauta-model';
 import { FilterDefinition, FiltersAndSorts, FILTER_TYPES } from '@nfa/next-sdr';
+import { Observable, of } from 'rxjs';
 
 
 @Component({
@@ -14,6 +15,8 @@ import { FilterDefinition, FiltersAndSorts, FILTER_TYPES } from '@nfa/next-sdr';
 export class ArchivioTreeComponent implements OnInit, TabComponent {
   @Input() data: any;
   public elements: any[] = [];
+  public selectedNode: TreeNode = null;
+  private ARCHIVIO_PLAIN_FIELD_PROJECTION: string = "ArchivioDetailWithPlainFields";
 
   constructor(private archivioService: ArchivioService) { }
 
@@ -26,32 +29,106 @@ export class ArchivioTreeComponent implements OnInit, TabComponent {
 
   getRenderedTreeNode(node: ArchivioDetail): TreeNode {
     const newNode: TreeNode = {};
+    newNode.key = node.id.toString();
     newNode.data = node;
-    newNode.label = node.numerazioneGerarchica
+    newNode.label = node.numerazioneGerarchica + ' ' + node.oggetto
     newNode.children = [];
+    newNode.expanded = true;
+
+    if (node.id === this.data['id']) {
+      this.selectedNode = newNode;
+    }
+
     node.archiviFigliList?.forEach(
       archivioFiglio => newNode.children.push(this.getRenderedTreeNode(archivioFiglio))
     )
+    node.archiviNipotiList?.forEach(inserto => {
+      newNode.children?.forEach(subfascicolo => {
+        if (inserto.fk_idArchivioPadre && subfascicolo.key === inserto.fk_idArchivioPadre.id.toString()) {
+          subfascicolo.children.push(this.getRenderedTreeNode(inserto))
+        }
+      })
+    });
+
     return newNode;
   }
+
+
+  getArchiviListByIdPadre(id: number): Promise<ArchivioDetail[]> {
+    let archivi: ArchivioDetail[] = null;
+    let filter: FiltersAndSorts = new FiltersAndSorts();
+    filter.addFilter(new FilterDefinition('fk_idArchivioPadre', FILTER_TYPES.not_string.equals, id));
+    return new Promise<ArchivioDetail[]>
+      (
+        (resolve, error) => this.archivioService.getData(
+          this.ARCHIVIO_PLAIN_FIELD_PROJECTION, filter, null, null)
+          .subscribe(
+            (res: { results: any[]; }) => {
+              resolve(res.results)
+            },
+            (err: { results: any[]; }) => {
+              error(err)
+            }
+          )
+      )
+  }
+
+
+  async getArchivioById(id: number): Promise<ArchivioDetail> {
+    let archivio: ArchivioDetail = null;
+    let filter: FiltersAndSorts = new FiltersAndSorts();
+    filter.addFilter(new FilterDefinition('id', FILTER_TYPES.not_string.equals, id));
+    return new Promise<ArchivioDetail>
+      (
+        (resolve, error) => this.archivioService.getData(
+          this.ARCHIVIO_PLAIN_FIELD_PROJECTION, filter, null, null)
+          .subscribe(
+            (res: { results: any[]; }) => {
+              resolve(res.results[0])
+            },
+            (err: { results: any[]; }) => {
+              error(err)
+            }
+          )
+      )
+  }
+
+  loadArchivi(): Promise<ArchivioDetail> {
+    return new Promise<ArchivioDetail>((archivio) => {
+      this.getArchivioById(this.data['id']).then(
+        res => {
+          console.log("loaded archivio res", res);
+          console.log("loading archivi by idPadre res['id']", res['id']);
+
+          this.getArchiviListByIdPadre(res['id']).then(
+            archivi => {
+              console.log("loaded figli: ", archivi);
+              res.archiviFigliList = [];
+              archivi.forEach(a => {
+                res.archiviFigliList.push(a);
+              })
+              archivio(res);
+            },
+            err => {
+              archivio(res);
+            }
+          )
+        },
+        err => archivio(null)
+      )
+    });
+  }
+
 
   loadDataByIdArchivio(): void {
     console.log("data", this.data);
     let loadedElements: any[] = [];
-    let filter: FiltersAndSorts = new FiltersAndSorts();
-    filter.addFilter(new FilterDefinition('id', FILTER_TYPES.not_string.equals, this.data['id']));
-
-    this.archivioService.getData("ArchivioDetailWithArchiviFigliListAndArchiviNipotiList", filter, null, null).subscribe(
-      (res: { results: any[]; }) => {
-        console.log("RES", res);
-        res.results.forEach((archive: ArchivioDetail) => {
-          const node = this.getRenderedTreeNode(archive)
-          loadedElements.push(node);
-        });
-        console.log("FINAL loadedElements", loadedElements);
-        this.elements = loadedElements
-      }
-    );
+    this.loadArchivi().then(res => {
+      console.log("FINAL RES", res);
+      const node = this.getRenderedTreeNode(res);
+      loadedElements.push(node);
+      this.elements = loadedElements;
+    });
   }
 
   fakeDataConstructor(): void {
