ea718b1e97e47d4b1cc25f3c00cbdd958a04a84d
f.gusella <devnull@localhost>
2022-03-08 18:04:30 +0100

    63953 Caption comune per più componenti

--- src/app/docs-list-container/docs-list/docs-list.module.ts
+++ src/app/docs-list-container/docs-list/docs-list.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { PrimeNgModule } from "../primeng.module";
-import { ScriptaCommonModule } from "../scripta-common.module";
+import { PrimeNgModule } from "../../primeng.module";
+import { ScriptaCommonModule } from "../../scripta-common.module";
 import { DocsListRoutingModule } from "./docs-list-routings.module";
 import { DocsListComponent } from "./docs-list.component";
 import { ExtendedDocDetailViewService } from "./extended-doc-detail-view.service";
