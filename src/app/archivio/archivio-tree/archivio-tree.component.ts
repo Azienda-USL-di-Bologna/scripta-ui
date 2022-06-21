a53cd5c22945c8044f9bba9ef26e490277955d87
lazzarini <c.lazzarini@nextsw.it>
2022-06-21 16:38:27 +0200

    pusho perchè non va

--- src/app/archivio/archivio-tree/archivio-tree.component.ts
+++ src/app/archivio/archivio-tree/archivio-tree.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MenuItem, TreeNode } from 'primeng/api';
 import { ExtendedArchivioService } from '../extended-archivio.service';
-import { Archivio, ArchivioDetail, ENTITIES_STRUCTURE } from '@bds/ng-internauta-model';
-import { combineLatest, Observable } from 'rxjs';
+import { Archivio, ArchivioDetail, ArchivioDetailViewService, ArchivioService, ConfigurazioneService, ENTITIES_STRUCTURE, ParametroAziende, UtenteService } from '@bds/ng-internauta-model';
+import { combineLatest, filter, Observable, Subscription } from 'rxjs';
 import { NavigationTabsService } from 'src/app/navigation-tabs/navigation-tabs.service';
 import { AppService } from 'src/app/app.service';
+import { FilterDefinition, FiltersAndSorts, FILTER_TYPES, PagingConf } from '@nfa/next-sdr';
+import { NtJwtLoginService, UtenteUtilities } from '@bds/nt-jwt-login';
 
 @Component({
   selector: 'archivio-tree',
@@ -16,6 +18,21 @@ export class ArchivioTreeComponent implements OnInit {
   //public bricioleArchivi: MenuItem[] = [];
   public selectedNode: TreeNode = null;
   private ARCHIVIO_PROJECTION: string = ENTITIES_STRUCTURE.scripta.archivio.customProjections.CustomArchivioWithIdAziendaAndIdMassimarioAndIdTitolo;
+  private ARCHIVIO_DETAIL_STANDARD: string = ENTITIES_STRUCTURE.scripta.archiviodetail.standardProjections.ArchivioDetailWithPlainFields;
+  private ARCHIVIO_DETAIL_PROJECTION = ENTITIES_STRUCTURE.scripta.archiviodetailview.customProjections.CustomArchivioDetailViewWithIdAziendaAndIdPersonaCreazioneAndIdPersonaResponsabileAndIdStrutturaAndIdVicari;
+
+  public troppiSottoFascicoli: boolean = false;
+  public troppiInserti: boolean = false;
+  public numeroMaxSottoarchiviCaricabili: number;
+  private utenteUtilitiesLogin: UtenteUtilities;
+
+  public pageConfNoLimit: PagingConf = {
+    conf: {
+      page: 0,
+      size: 999999
+    },
+    mode: "PAGE_NO_COUNT"
+  };
 
   @Output() archivioSelectedEvent = new EventEmitter<ArchivioDetail>();
 
@@ -36,24 +53,47 @@ export class ArchivioTreeComponent implements OnInit {
    * Il nome verrà oscurato lato dall'after select interceptor nel backend.
    */
   constructor(
+    private configurationService: ConfigurazioneService,
     private archivioService: ExtendedArchivioService,
+    private achivioDetailViewService: ArchivioDetailViewService,
+    private loginService: NtJwtLoginService,
     private navigationTabsService: NavigationTabsService,
     private appService: AppService,
     ) {
-
+     
   }
 
   ngOnInit(): void {
     //console.log("ArchivioTreeComponent.ngOnInit()", this.archivio);
-    this.firstTreeLoad();
+    this.loadConfig();
   }
 
   //Todo: BreadCrumbs is not used for now but the code for it is still present (bricioleArchivi is commented)
 
   /**
+   * Questo metodo si occupa di caricare il numero massimo di fascicoli caricabili
+   * dipendente dall'azienda di appartenenza del fascicolo
+   */
+  private loadConfig(): void {
+    this.loginService.loggedUser$.subscribe(
+      (utenteUtilities: UtenteUtilities) => {
+        this.utenteUtilitiesLogin = utenteUtilities;
+      }
+    );
+    this.configurationService.getParametriAziende("numeroMaxSottoarchiviCaricabili", null, [this.archivio.fk_idAzienda.id])
+      .subscribe( (parametriAziende: ParametroAziende[]) => {
+        if (parametriAziende && parametriAziende[0]) {
+          this.numeroMaxSottoarchiviCaricabili = parametriAziende[0].valore as number;
+          this.firstTreeLoad();
+        }
+      })
+  }
+
+  /**
    * Questo metodo si occupa di caricare l'alberatura iniziale.
    * In particolare, se ho aperto un sottofascicolo/inserto allora 
-   * devo caricare il padre e l'eventuale nonno.
+   * devo caricare il padre e l'eventuale nonno e i fratelli. 
+   * Di un fascicolo devo caricare i sottofascicoli
    */
   private firstTreeLoad(): void {
     // this.bricioleArchivi.push({
@@ -61,40 +101,188 @@ export class ArchivioTreeComponent implements OnInit {
     // } as any);
     switch (this.archivio.livello) {
       case 1:
-        this.archivi.push(this.buildTreeNode(this.archivio));
+        /**this.archivi.push(this.buildTreeNode(this.archivio));
         //  this.bricioleArchivi.push(this.buildMenuItem(this.archivio));
+        break;*/
+        const archivioRadice = this.archivio as Archivio;
+        if (archivioRadice.numeroSottoarchivi < this.numeroMaxSottoarchiviCaricabili) {
+          const filtersAndSorts =  this.buildFilterToLoadChildren(archivioRadice)
+          this.achivioDetailViewService.getData(this.ARCHIVIO_DETAIL_PROJECTION, filtersAndSorts, null, this.pageConfNoLimit)
+            .subscribe(
+              (res) => {
+                let children = res.results;
+                let childrenNodes: TreeNode<any>[] = [];
+                for (let i = 0; i < children.length ; i++ ) {
+                  childrenNodes.push(this.buildTreeNode(children[i]))
+                }
+                this.archivi.push(this.buildTreeNode(this.archivio, childrenNodes))
+              }
+            )
+        } else {
+          this.troppiSottoFascicoli = true;
+          this.archivi.push(this.buildTreeNode(this.archivio));
+        }
         break;
       case 2:
         this.getArchivioById(this.archivio.fk_idArchivioPadre.id).subscribe(
-          archivioPadre => {
-            this.archivi.push(
-              this.buildTreeNode(archivioPadre, [
-                this.buildTreeNode(this.archivio)
-              ]));
-            // this.bricioleArchivi.push(this.buildMenuItem(archivioPadre));
-            // this.bricioleArchivi.push(this.buildMenuItem(this.archivio));
-            // this.bricioleArchivi = [...this.bricioleArchivi];
+          archivioPa => {
+            const archivioPadre: Archivio = archivioPa as Archivio;
+            if(archivioPadre && archivioPadre.numeroSottoarchivi < this.numeroMaxSottoarchiviCaricabili) {
+              const filtersAndSorts =  this.buildFilterToLoadChildren(archivioPadre)
+              this.achivioDetailViewService.getData(this.ARCHIVIO_DETAIL_PROJECTION, filtersAndSorts, null, this.pageConfNoLimit)
+                .subscribe(
+                  (res) => {
+                    console.log("ecco ciò che carico" , res.results)
+                    let brothers = res.results;
+                    let brothersNodes: TreeNode<any>[] = [];
+                    for (let i = 0; i < brothers.length ; i++ ) {
+                      brothersNodes.push(this.buildTreeNode(brothers[i]))
+                    }
+                    this.archivi.push(this.buildTreeNode(archivioPadre, brothersNodes));
+                  }
+                )
+              }
+              else {
+                this.troppiSottoFascicoli = true;
+                this.archivi.push(this.buildTreeNode(archivioPadre, [this.buildTreeNode(this.archivio)]));
+              }
           }
         );
+        // this.getArchivioById(this.archivio.fk_idArchivioPadre.id).subscribe(
+        //   archivioPadre => {
+        //     this.archivi.push(
+        //       this.buildTreeNode(archivioPadre, [
+        //         this.buildTreeNode(this.archivio)
+        //       ]));
+        //     // this.bricioleArchivi.push(this.buildMenuItem(archivioPadre));
+        //     // this.bricioleArchivi.push(this.buildMenuItem(this.archivio));
+        //     // this.bricioleArchivi = [...this.bricioleArchivi];
+        //   }
+        // );
         break;
       case 3:
         combineLatest([
-          this.getArchivioById(this.archivio.fk_idArchivioPadre.id),
-          this.getArchivioById(this.archivio.fk_idArchivioRadice.id)
-        ]).subscribe(
-          ([archivioPadre, archivioNonno]) => {
-            this.archivi.push(
-              this.buildTreeNode(archivioNonno, [
-                this.buildTreeNode(archivioPadre, [
-                  this.buildTreeNode(this.archivio)
-                ])
+            this.getArchivioById(this.archivio.fk_idArchivioPadre.id),
+            this.getArchivioById(this.archivio.fk_idArchivioRadice.id)
+          ]).subscribe(
+            ([archivioPadre, archivioNonno ] ) => {
+              if(archivioNonno.numeroSottoarchivi <= this.numeroMaxSottoarchiviCaricabili && archivioPadre.numeroSottoarchivi <= this.numeroMaxSottoarchiviCaricabili) {
+                const filtersAndSorts =  this.buildFilterToLoadChildren(archivioNonno)
+                this.achivioDetailViewService.getData(this.ARCHIVIO_DETAIL_PROJECTION, filtersAndSorts, null, this.pageConfNoLimit)
+                .subscribe(
+                  (res) => {
+                    let uncles = res.results;
+                    let unclesNodes: TreeNode<any>[] = [];
+                    let fatherAndSons: TreeNode<any>[] = [];
+                    for (let i = 0; i < uncles.length ; i++ ) {
+                      if(uncles[1].id == archivioPadre.id) {
+                        const filtersAndSorts =  this.buildFilterToLoadChildren(archivioPadre)
+                        this.achivioDetailViewService.getData(this.ARCHIVIO_DETAIL_PROJECTION, filtersAndSorts, null, this.pageConfNoLimit)
+                        .subscribe(
+                          (res) => {
+                            let children = res.results;
+                            let childrenNodes: TreeNode<any>[] = [];
+                            for (let i = 0; i < children.length ; i++ ) {
+                              childrenNodes.push(this.buildTreeNode(children[i]))
+                            }
+                            fatherAndSons.push(
+                                this.buildTreeNode(archivioPadre, [
+                                  this.buildTreeNode(this.archivio)
+                                ]
+                              )
+                            );
+                          }
+                        )
+                      } else {
+                        unclesNodes.push(this.buildTreeNode(uncles[i]))
+                      }
+                      unclesNodes = unclesNodes.concat(fatherAndSons)
+                      this.archivi.push(this.buildTreeNode(archivioNonno, unclesNodes))
+                    }
+                  }
+                ) 
+              }
+              else if (archivioNonno.numeroSottoarchivi <= this.numeroMaxSottoarchiviCaricabili && archivioPadre.numeroSottoarchivi > this.numeroMaxSottoarchiviCaricabili) {
+                const filtersAndSorts =  this.buildFilterToLoadChildren(archivioNonno)
+                this.achivioDetailViewService.getData(this.ARCHIVIO_DETAIL_PROJECTION, filtersAndSorts, null, this.pageConfNoLimit)
+                .subscribe(
+                  (res) => {
+                    let uncles = res.results;
+                    let unclesNodes: TreeNode<any>[] = [];
+                    let fatherAndSons: TreeNode<any>[] = [];
+                    for (let i = 0; i < uncles.length ; i++ ) {
+                      unclesNodes.push(this.buildTreeNode(uncles[i]))
+                      unclesNodes = unclesNodes.concat(fatherAndSons);
+                      this.archivi.push(this.buildTreeNode(archivioNonno, unclesNodes))
+                    }
+                    this.troppiInserti = true;
+                  }
+                )
+              }
+              else if (archivioNonno.numeroSottoarchivi > this.numeroMaxSottoarchiviCaricabili && archivioPadre.numeroSottoarchivi <= this.numeroMaxSottoarchiviCaricabili) {
+              let fatherAndSons: TreeNode<any>[] = [];
+                this.troppiSottoFascicoli = true;
+                const filtersAndSorts =  this.buildFilterToLoadChildren(archivioPadre)
+                this.achivioDetailViewService.getData(this.ARCHIVIO_DETAIL_PROJECTION, filtersAndSorts, null, this.pageConfNoLimit)
+                .subscribe(
+                  (res) => {
+                    let children = res.results;
+                    let childrenNodes: TreeNode<any>[] = [];
+                    for (let i = 0; i < children.length ; i++ ) {
+                      childrenNodes.push(this.buildTreeNode(children[i]))
+                    }
+                    fatherAndSons.push(
+                        this.buildTreeNode(archivioPadre, [
+                          this.buildTreeNode(this.archivio)
+                        ]
+                      )
+                    );
+                  }
+                )
+              this.archivi.push(this.buildTreeNode(archivioNonno, fatherAndSons))
+            }
+            else {
+              this.archivi.push(
+                  this.buildTreeNode(archivioNonno, [
+                    this.buildTreeNode(archivioPadre, [
+                      this.buildTreeNode(this.archivio)
+                    ])
               ]));
-            // this.bricioleArchivi.push(this.buildMenuItem(archivioNonno));
-            // this.bricioleArchivi.push(this.buildMenuItem(archivioPadre));
-            // this.bricioleArchivi.push(this.buildMenuItem(this.archivio));
-            // this.bricioleArchivi = [...this.bricioleArchivi];
-          }
-        );
+              this.troppiInserti = true;
+              this.troppiSottoFascicoli = true;
+            }
+                 
+              
+              // this.archivi.push(
+              //   this.buildTreeNode(archivioNonno, [
+              //     this.buildTreeNode(archivioPadre, [
+              //       this.buildTreeNode(this.archivio)
+              //     ])
+              //   ]));
+              // this.bricioleArchivi.push(this.buildMenuItem(archivioNonno));
+              // this.bricioleArchivi.push(this.buildMenuItem(archivioPadre));
+              // this.bricioleArchivi.push(this.buildMenuItem(this.archivio));
+              // this.bricioleArchivi = [...this.bricioleArchivi];
+            }
+          );
+
+        // combineLatest([
+        //   this.getArchivioById(this.archivio.fk_idArchivioPadre.id),
+        //   this.getArchivioById(this.archivio.fk_idArchivioRadice.id)
+        // ]).subscribe(
+        //   ([archivioPadre, archivioNonno]) => {
+        //     this.archivi.push(
+        //       this.buildTreeNode(archivioNonno, [
+        //         this.buildTreeNode(archivioPadre, [
+        //           this.buildTreeNode(this.archivio)
+        //         ])
+        //       ]));
+        //     // this.bricioleArchivi.push(this.buildMenuItem(archivioNonno));
+        //     // this.bricioleArchivi.push(this.buildMenuItem(archivioPadre));
+        //     // this.bricioleArchivi.push(this.buildMenuItem(this.archivio));
+        //     // this.bricioleArchivi = [...this.bricioleArchivi];
+        //   }
+        // );
         break;
     }
   }
@@ -165,6 +353,43 @@ export class ArchivioTreeComponent implements OnInit {
     }
   }
 
+
+/**
+ * Metodo che serve per poter caricare gli inserti di un sottofascicolo qualora venisse selezionato
+ * dall'albero, ma vengono caricati solo se sono meno del numero predefinito
+ * @param archivio 
+ * @returns 
+ */
+  private loadChildren(archivio: Archivio | ArchivioDetail): void {
+    if (archivio.livello === 2 && archivio.numeroSottoarchivi <= this.numeroMaxSottoarchiviCaricabili) {
+      const filtersAndSorts = this.buildFilterToLoadChildren(archivio);
+      this.achivioDetailViewService.getData(this.ARCHIVIO_DETAIL_PROJECTION, filtersAndSorts, null, this.pageConfNoLimit).subscribe(
+        res => {
+          let archiviFigli = res.results;
+          const indexLiv2 = this.archivi[0].children.findIndex(nodo => (nodo.data as Archivio).id === archivio.id);
+          for (let i = 0; i < archiviFigli.length ; i++ ) {
+            if(!(this.archivi[0].children[indexLiv2].children.find(nodo => (nodo.data as Archivio).id === archiviFigli[i].id))) {
+              this.archivi[0].children[indexLiv2].children.push(this.buildTreeNode(archiviFigli[i]))
+            }
+          }
+        }
+      )
+    } else if (archivio.livello === 2 && archivio.numeroSottoarchivi > this.numeroMaxSottoarchiviCaricabili) {
+      console.log("troppi inserti ")
+      this.troppiInserti = true;
+    }
+  }
+
+
+
+  private buildFilterToLoadChildren(archivio: Archivio | ArchivioDetail):  FiltersAndSorts {
+    const filtersAndSorts: FiltersAndSorts = new FiltersAndSorts();
+    filtersAndSorts.addFilter(new FilterDefinition("idArchivioPadre.id", FILTER_TYPES.not_string.equals, archivio.id));
+    filtersAndSorts.addFilter(new FilterDefinition("idAzienda.id", FILTER_TYPES.not_string.equals,archivio.fk_idAzienda.id));
+    filtersAndSorts.addFilter(new FilterDefinition("idPersona.id", FILTER_TYPES.not_string.equals, this.utenteUtilitiesLogin.getUtente().idPersona.id));
+    return filtersAndSorts;
+  }
+
   /**
    * Ritorno l'observable per il get di un archivio
    * @param idArchivio 
@@ -214,6 +439,7 @@ export class ArchivioTreeComponent implements OnInit {
    * @param event 
    */
   public onNodeSelect(event: any): void {
+    this.loadChildren(event.node.data);
     this.navigationTabsService.addTabArchivio(event.node.data, false);
     this.appService.appNameSelection("Fascicolo "+ event.node.data.numerazioneGerarchica  + " [" + event.node.data.idAzienda.aoo + "]");
     //this.archivioSelectedEvent.emit(event.node.data);
