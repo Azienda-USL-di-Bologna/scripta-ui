636d65e938938559ea15c6f28501adf094dfc082
Giuseppe Russo <g.russo@dilaxia.com>
2023-07-26 20:03:24 +0200

    BCD-105 Fix vari, aggiunto responsabile di procedimento

--- src/app/doc/attori/attori.component.ts
+++ src/app/doc/attori/attori.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Doc } from '@bds/internauta-model';
+import { AttoreDoc, Doc, RuoloAttoreDoc } from '@bds/internauta-model';
 
 @Component({
   selector: 'attori',
@@ -10,13 +10,21 @@ export class AttoriComponent {
   
   @Input() set doc(value: Doc) {
     this._doc = value;
+    this.setResponsabileProcedimento(value);
   }
   get doc(): Doc {
     return this._doc;
   }
-
+  
+  // Uso un array per usare la listbox ed evitare di lavorare sul css per rendere i campi uguali
+  responsabileProcedimentoAsList: AttoreDoc[];  
   private _doc: Doc;
-
+  
   constructor() { }
+
+  setResponsabileProcedimento(doc: Doc) {
+    if (doc && doc.attoriList)
+      this.responsabileProcedimentoAsList = doc.attoriList.filter(a => a.ruolo === RuoloAttoreDoc.RESPONSABILE_PROCEDIMENTO);
+  }
 
 }
